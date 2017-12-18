940524a381088a4ff592bb4395f56e177b91c884
Yuta Yamamoto <tuki0918@gmail.com>
2017-12-19 07:46:10 +0900

    ViewにNative Moduleの結果を反映

--- AwesomeProject/index.js
+++ AwesomeProject/index.js
@@ -1,9 +1,4 @@
-import { AppRegistry, NativeModules } from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-
-const RNCoreML = NativeModules.RNCoreML;
-RNCoreML.addEvent('ABC').then(name => {
-    console.log(name);
-});
 
 AppRegistry.registerComponent('AwesomeProject', () => App);
